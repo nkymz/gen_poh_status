59c42b5652065e44d2766779fb8303716319bc8b
nkymz <nakayama7322@gmail.com>
2018-10-18 13:04:20 +0900

    WIP

--- gen_poh_status.py
+++ gen_poh_status.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import requests
+import os
+import time
+import datetime
+
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import openpyxl
+
+
+POGPATH = os.getenv("HOMEDRIVE", "None") + os.getenv("HOMEPATH", "None") + "/Dropbox/POG/"
+WBPATH = (POGPATH + "POG_HorseList.xlsx").replace("\\", "/")
+HTMLPATH = (POGPATH + "next_race_list.html").replace("\\", "/")
+WEBDRIVERPATH = r"C:\Selenium\chromedriver.exe"
+
+JRA_DF_URL = "http://www.jra.go.jp/datafile/"
+
+mytoday = datetime.date.today()
+
 
 HDB_URL = "http://www.jra.go.jp/JRADB/accessR.html"
 DICT_POST_HDB = {"cname": "pw02uliD19999"}
@@ -15,12 +33,18 @@ DICT_SEARCH_FORM = {
 
 
 def main():
-    mysession = requests.Session()
-    r = mysession.post("http://www.jra.go.jp/JRADB/accessR.html", data=DICT_POST_HDB)
-    r = mysession.post("http://www.jra.go.jp/JRADB/accessR.html", data=DICT_SEARCH_FORM)
-    soup = BeautifulSoup(r.content, "lxml")
-    print(soup.prettify)
+    options = Options()
+    # options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path=WEBDRIVERPATH, options=options)
 
+    time.sleep(1)
+    driver.get(JRA_DF_URL)
+    time.sleep(1)
+    # elm = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div[1]/ul[1]/li[3]/a[1]").click()
+    elm = driver.find_element_by_xpath("//img[@alt='競走馬検索']/../..")
+    print(elm.get_attribute("onclick"))
+    elm.click()
+    time.sleep(5)
 
 if __name__ == "__main__":
     main()
