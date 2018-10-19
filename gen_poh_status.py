2cfd276c265779498bcc1693f2afe7b62a7c35dd
nkymz <nakayama7322@gmail.com>
2018-10-19 13:02:20 +0900

    WIP

--- gen_poh_status.py
+++ gen_poh_status.py
@@ -3,6 +3,7 @@
 import os
 import time
 import datetime
+import re
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -40,13 +41,27 @@ def main():
     time.sleep(1)
     driver.get(JRA_DF_URL)
     time.sleep(1)
-    # elm = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div[1]/ul[1]/li[3]/a[1]").click()
-    elm = driver.find_element_by_xpath("//img[@alt='競走馬検索']/../..")
-    print(elm.get_attribute("onclick"))
-    elm.click()
+    driver.find_element_by_xpath("//img[@alt='競走馬検索']/../..").click()
+    time.sleep(1)
+    driver.find_element_by_xpath("//td[contains(text(), '競走馬名')]/following-sibling::td[1]/input").send_keys("ビーチサンバ")
+    driver.find_element_by_xpath("//td[contains(text(), '競走馬名')]/following-sibling::td[1]/a").click()
+    time.sleep(1)
+    driver.find_element_by_xpath("//a[contains(text(), 'ビーチサンバ')]").click()
+    time.sleep(1)
+    if driver.find_elements_by_xpath("//td[contains(text(), '放牧')]"):
+        print("放牧")
+    else:
+        print("放牧じゃない")
+    # print(driver.find_elements_by_xpath("//a[contains(text(), 'ビーチサンバ')]/..")[1].text)
+    # print(driver.find_elements_by_xpath("//a[contains(text(), '*ビーチサンバ')]/..")[1].text)
+    # for i in driver.find_elements_by_xpath("//a/.."):
+    #    print(i.text)
+
+
     time.sleep(5)
 
 if __name__ == "__main__":
     main()
 
 # 中央未登録、中央登録、剤級、放牧、中央抹消、地方、海外
+
